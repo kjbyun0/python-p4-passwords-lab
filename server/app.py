b5ab04ceaea8552787dcf283f6984da3403ec340
KI JONG BYUN <kjbyun0@gmail.com>
2024-03-12 13:23:39 -0500

    Assignment Done

--- server/app.py
+++ server/app.py
@@ -28,16 +28,36 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter_by(id=user_id).first()
+            return user.to_dict(), 200
+        return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        user = User.query.filter_by(username=json['username']).first()
+        if user:
+            if user.authenticate(json['password']):
+                session['user_id'] = user.id
+                return user.to_dict(), 200
+            return {'message': 'Invalid password'}, 401
+        return {'message': "Invalid username"}, 401
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {}, 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='checksession')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
